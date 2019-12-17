4e4ddb5aa861e47890bdc43f145268227ce11d08
Tomar <rakesh.tomar@capgemini.com>
2019-12-17 15:48:48 +0530

    Adding

--- main.tf
+++ main.tf
@@ -152,6 +152,36 @@ resource "aws_codebuild_project" "test_project" {
   }
 }
 
+# create a CodeDeploy application
+resource "aws_codedeploy_app" "main" {
+  name = var.repo_name
+}
+
+# create a deployment group
+resource "aws_codedeploy_deployment_group" "main" {
+  app_name              = "${aws_codedeploy_app.main.name}"
+  deployment_group_name = "Sample_DepGroup"
+  service_role_arn      = "${aws_iam_role.codedeploy_service.arn}"
+
+  deployment_config_name = "CodeDeployDefault.OneAtATime" # AWS defined deployment config
+
+  ec2_tag_filter = {
+    key   = "Name"
+    type  = "KEY_AND_VALUE"
+    value = "devlopment"
+  }
+
+  # trigger a rollback on deployment failure event
+  auto_rollback_configuration {
+    enabled = true
+    events = [
+      "DEPLOYMENT_FAILURE",
+    ]
+  }
+}
+
+
+
 # Full CodePipeline
 resource "aws_codepipeline" "codepipeline" {
   name     = var.repo_name
