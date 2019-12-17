ba09dd78554647dae2bc71913c073d76b1d9d0b0
Rakesh Singh Tomar <singh.rakesh406@gmail.com>
2019-12-17 18:41:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -106,7 +106,7 @@ resource "aws_iam_role_policy" "codebuild_policy" {
 
 # CodeBuild Section for the Build stage
 resource "aws_codebuild_project" "build_project" {
-  name           = "${var.repo_name}-Build`"
+  name           = "${var.repo_name}-Build"
   description    = "The CodeBuild project for ${var.repo_name}"
   service_role   = aws_iam_role.codebuild_assume_role.arn
   build_timeout  = var.build_timeout
