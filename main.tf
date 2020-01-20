89e341d1ed6acbd74f6c09c4638389de645c6d41
Tomar <rakesh.tomar@capgemini.com>
2020-01-20 19:04:04 +0530

    Adding changes

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ module "unique_label" {
 
 # Create ECR repo
 resource "aws_ecr_repository" "foo" {
-  name                 =  "ecrdemotest"
+  name                 = var.repo_name 
   image_tag_mutability = "IMMUTABLE"
   image_scanning_configuration {
     scan_on_push = true
