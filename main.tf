7fcc2a7aa73083ac1c2264adc2d5843a4f86a6df
Rakesh Singh Tomar <singh.rakesh406@gmail.com>
2020-03-16 13:02:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -133,18 +133,16 @@ EOF
 }
 
 # provide ec2 access to s3 bucket to download revision. This role is needed by the CodeDeploy agent on EC2 instances.
-resource "aws_iam_role_policy_attachment" "instance_profile_codedeploy" {
-  role       = "${aws_iam_role.instance_profile.name}"
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforAWSCodeDeploy"
-}
-
 resource "aws_iam_instance_profile" "main" {
   name = "codedeploy-instance-profile"
   role = "${aws_iam_role.instance_profile.name}"
 }
-
-
-
+  
+  
+resource "aws_iam_role_policy_attachment" "instance_profile_codedeploy" {
+  role       = "${aws_iam_role.instance_profile.name}"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforAWSCodeDeploy"
+}
 
 
 # Encryption key for build artifacts
